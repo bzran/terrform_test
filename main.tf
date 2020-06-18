37e3e6f2870d8903be0b0d342bc68cd40a2570b3
weiyunhe <weiyunhe@B-Q006J1WT-1155.local>
2020-06-18 14:12:38 +0800

    ceshi

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "alicloud_image" "default" {
   instance_id        = "i-uf6b9se8bpy28pbf4gvv"
-  image_name         = "test-image2"
+  image_name         = "test-image3"
   description        = "test-image"
   tags = {
     FinanceDept = "test_terrform"
