b66136e4345f9324a7e8dce7a6ee58e63c6a8776
bzran <zhengran.bzr@alibaba-inc.com>
2020-09-27 12:51:49 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "alicloud_image" "default" {
   instance_id        = "i-uf6b9se8bpy28pbf4gvv"
   image_name         = "test-image2"
   description        = "test-image"
-  tags = {1111
+  tags = {
     FinanceDept = "test_terrform"
   }
 }
